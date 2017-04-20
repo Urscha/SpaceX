dc89a0b80d4b72f4537dc0c55ddc6dcba27e8d2e
Ricoter <Ricoter@users.noreply.github.com>
2017-04-20 15:05:59 +0200

    Add files via upload

--- CorgoOne_b/mainB.py
+++ CorgoOne_b/mainB.py
@@ -4,8 +4,6 @@ import matplotlib
 import sys
 from operator import itemgetter
 
-# initialize maximal score
-score = 10786.35
 
 # read data with additional values made in excell (ratio )
 def read_data(input_file):
@@ -17,56 +15,88 @@ def read_data(input_file):
                 if l%5==0:
                     t = (int(elements[l]), int(elements[l+1]), float(elements[l+2]), float(elements[l+3]), float(elements[l+4]))
                     data.append(t)
+    return data
+
+
+def kg_left(s):
+    return s[5] - sum(item[1] for item in s[4])
+
+
+def m3_left(s):
+    return s[6] - sum(item[2] for item in s[4])
+
+
+def update_ship(s):
+    s[1] = kg_left(s)
+    s[2] = m3_left(s)
+    s[3] = float(s[1]) / float(s[2])
+
+
+def which_ship_ratio(ships, item):
+    ratio = item[4]
+    s0 = (0, abs(ships[0][3] - ratio))
+    s1 = (1, abs(ships[1][3] - ratio))
+    s2 = (2, abs(ships[2][3] - ratio))
+    s3 = (3, abs(ships[3][3] - ratio))
+    sList = [s0, s1, s2, s3]
+    sList.sort(key=itemgetter(1))
+    for s in sList:
+        if ships[s[0]][1]>item[1] and ships[s[0]][2]>item[2]:
+            return s[0]
+
+    return -1
+
 
 # initiale ships, including their mass/volume ratio
 def fill_cargo_ratio(ships, data):
     # sort packages: highest score on top
     data.sort(key=itemgetter(3), reverse=True)
-
     # put package with highest score in ship with closest kg/m3 ratio
     for i in data:
-        if i[4] < 140.7 and (ships[0][1] - i[1]) >= 0 and (ships [0][2] - i[2]) >= 0:
-            ships[0][1] -= i[1]
-            ships[0][2] -= i[2]
-            print "packing " , i[0] , " in ship " ,  ships[0][0]
-        elif i[4] < 223.5 and (ships[1][1] - i[1]) >= 0 and (ships [1][2] - i[2]) >= 0:
-            ships[1][1] -= i[1]
-            ships[1][2] -= i[2]
-            print "packing " , i[0] , " in ship " ,  ships[1][0]
-        elif i[4] < 587.22 and (ships[3][1] - i[1]) >= 0 and (ships [3][2] - i[2]) >= 0:
-            ships[3][1] -= i[1]
-            ships[3][2] -= i[2]
-            print "packing " , i[0] , " in ship " ,  ships[3][0]
-        elif (ships[2][1] - i[1]) >= 0 and (ships [2][2] - i[2]) >= 0:
-            ships[2][1] -= i[1]
-            ships[2][2] -= i[2]
-            print "packing " , i[0] , " in ship " ,  ships[2][0]
-        # see if leftover packages fit in ships
-        elif (ships[1][1] - i[1]) >= 0 and (ships [1][2] - i[2]) >= 0:
-            ships[1][1] -= i[1]
-            ships[1][2] -= i[2]
-            print "packing " , i[0] , " in ship " ,  ships[1][0]
-        else:
-            score -= i[3]
-            print "could not pack ", i[0]
+        s = which_ship_ratio(ships, i)
+        if s == -1:
+            continue
+        ships[s][4].append(i)
+        update_ship(ships[s])
 
 
-def print_ships(ships, cargo=False, errorcheck=False):
+def print_ships(ships, score=True, cargo=False, errorcheck=False):
+    svalue = 0
     for i in ships:
         print i[0], "\t kg: ", kg_left(i), "\t m3: ", m3_left(i)
         if(cargo):
             print "cargo: ", i[3], "\n"
         if(errorcheck):
             print i[1], i[2], "\n"
+        if(score):
+            svalue += (i[1]/i[5] + i[2]/i[6])*(100/2)
+    if(score):
+        print "average percentage filled: ",100 - svalue/4.0,"%\n"
+
+
+def init_ships():
+    ships = [["Cygnus   ", 2000.0, 18.9, 0.0, [], 2000.0, 18.9],
+             ["Verne_ATV", 2300.0, 13.1, 0.0, [], 2300.0, 13.1],
+             ["Progress ", 2400.0,  7.6, 0.0, [], 2400.0,  7.6],
+             ["Kounotori", 5200.0, 14.0, 0.0, [], 5200.0, 14.0]]
+
+    for s in ships:
+        update_ship(s)
+    return ships
 
 #                           _____MAIN_____
 def main():
-    ships = [["Cygnus", 2000, 18.9, 105.82,[]], ["Verne_ATV", 2300, 13.1, 175.57,[]], ["Progress", 2400, 7.6, 315.79,[]], ["Kounotori",5200, 14, 271.43,[]]]
-
+    # initiale ships
+    ships = init_ships()
     cargolist = read_data("CargoList1_b.txt")
     fill_cargo_ratio(ships, cargolist)
     print_ships(ships)
-    print "score = ", score, "\n"
+
+    ships2 = init_ships()
+    cargolist = read_data("CargoList1_b2.txt")
+    fill_cargo_ratio(ships2, cargolist)
+    print_ships(ships2)
+
 
 
 if __name__ == "__main__":
